b21beb3328b883115f8ff18b01095cfe2d8c2f06
Maddy398 <madhupsharma398@gmail.com>
2025-03-31 03:27:24 +0530

    I part completed

---
+++ models/listing.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+const listingSchema = new schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        filename: String,
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+            set: (v) => v === "" ? "https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wfGVufDB8fDB8fA%3D%3D&w=1000&q=80" : v
+        }
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
